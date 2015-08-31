d969b4db71d7023183735fd2225be554c1cb22ac
darciga <dante.omar.arciga@gmail.com>
2015-08-30 23:32:27 -0500

    include new js files and add handling CSRF protection

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -1,2 +1,16 @@
 angular
-  .module('thinkster', []);
+    .module('thinkster', []);
+
+angular
+    .module('thinkster')
+    .run(run);
+run.$inject = ['$http'];
+/**
+ * @name run
+ * @desc Update xsrf $http headers to align with Django's defaults
+ */
+function run($http) {
+    $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+    $http.defaults.xsrfCookieName = 'csrftoken';
+}
+

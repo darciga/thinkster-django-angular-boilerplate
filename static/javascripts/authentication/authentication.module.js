a7cb2434eaeb807a60b7515e417d4e5f9373fe74
darciga <dante.omar.arciga@gmail.com>
2015-08-30 23:06:10 -0500

    Setting up AngularJS module authentication

---
+++ static/javascripts/authentication/authentication.module.js
@@ -0,0 +1,15 @@
+/**
+ * Created by darciga on 30/08/15.
+ */
+(function () {
+    'use strict';
+    angular
+        .module('thinkster.authentication'[
+            'thinkster.authentication.controllers',
+                'thinkster.authentication.services'
+            ]);
+    angular
+        .module('thinkster.authentication.controllers', []);
+    angular
+        .module('thinkster.authentication.services', ['ngCookies']);
+})();
